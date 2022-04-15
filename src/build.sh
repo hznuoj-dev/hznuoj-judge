676e726c8e9c2406a67ede7d98c6cf9d983109ae
Dup4 <lyuzhi.pan@gmail.com>
2022-04-15 20:52:37 +0800

    fix: clean sim_gen.c before compile

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 BUILD_SH_TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+SIM_DIR="${BUILD_SH_TOP_DIR}/sim/sim_3_0_2"
 
 cd "${BUILD_SH_TOP_DIR}/judged" || exit 1
 
@@ -12,9 +13,14 @@ cd "${BUILD_SH_TOP_DIR}/judge_client" || exit 1
 make
 chmod +x judge_client
 
-cd "${BUILD_SH_TOP_DIR}/sim/sim_3_0_2" || exit 1
+cd "${SIM_DIR}" || exit 1
 
 make fresh
+
+if [[ -f "${SIM_DIR}/sim_gen.c" ]]; then
+  rm "${SIM_DIR}/sim_gen.c"
+fi
+
 make exes
 
 chmod +x sim_*.exe
